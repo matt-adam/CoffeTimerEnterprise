80e519635c91778b549779ff6a76d306e8205268
buddybuild <help@buddybuild.com>
2016-11-02 16:37:45 -0700

    Adding buddybuild SDK
    - Add BuddyBuildSDK.framework
    - Initialize in App Delegate

--- Coffee Timer/Coffee Timer/AppDelegate.swift
+++ Coffee Timer/Coffee Timer/AppDelegate.swift
@@ -26,6 +26,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
+        BuddyBuildSDK.setup()
+        
         print("Application has launched.")
 
         coreDataStack.loadDefaultDataIfFirstLaunch()
